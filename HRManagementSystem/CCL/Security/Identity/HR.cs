67ff8741bbfb4bf432dd62d9f35c407479e607ef
Tania Holub <taniagolub2004@gmail.com>
2024-12-10 21:36:03 +0200

    add CCL impl

---
+++ HRManagementSystem/CCL/Security/Identity/HR.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace CCL.Security.Identity
+{
+    public class HR
+     : User
+    {
+        public HR (int userId, string name)
+        : base(userId, name, nameof(HR))
+        {
+
+        }
+    }
+}
