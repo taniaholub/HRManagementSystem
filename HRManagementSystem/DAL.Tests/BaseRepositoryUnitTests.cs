450739faabc8f5aa6466b41470f12d8ce6da48d6
Tania Holub <taniagolub2004@gmail.com>
2024-12-10 10:48:07 +0200

    add BaseRepositoryUnitTests

---
+++ HRManagementSystem/DAL.Tests/BaseRepositoryUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using DAL.EF;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Moq;
+using Xunit;
+
+namespace DAL.Tests
+{
+    public class BaseRepositoryUnitTests
+    {
+        [Fact]
+        public void MockContextTest()
+        {
+            // Створення макету DbSet
+            var mockDbSet = new Mock<DbSet<User>>();
+
+            // Створення макету DbContext
+            var mockContext = new Mock<HRManagementSystemContext>();
+            mockContext.Setup(c => c.Set<User>()).Returns(mockDbSet.Object);
+
+            // Перевірка
+            Assert.NotNull(mockContext.Object);
+        }
+    }
+}
