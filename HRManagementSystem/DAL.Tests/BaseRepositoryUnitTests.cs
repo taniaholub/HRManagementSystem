2424c2b341f7ab019352d4ebefa1fd26ec5fca1f
Tania Holub <taniagolub2004@gmail.com>
2024-12-11 13:04:38 +0200

    fix DAL.Tests

--- HRManagementSystem/DAL.Tests/BaseRepositoryUnitTests.cs
+++ HRManagementSystem/DAL.Tests/BaseRepositoryUnitTests.cs
@@ -1,115 +1,122 @@
 ﻿using DAL.EF;
 using DAL.Entities;
+using DAL.Enums;
 using DAL.Repositories.Implementation;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Moq;
+using System.IO;
+using Xunit;
 
 namespace DAL.Tests
 {
-    public class BaseRepositoryUnitTests
+    namespace DAL.Tests
     {
-        [Fact]
-        public void Create_InputUserInstance_CalledAddMethodOfDBSetWithUserInstance()
+        public class BaseRepositoryUnitTests
         {
-            // Arrange: організація тестових даних
-            DbContextOptions opt = new DbContextOptionsBuilder<HRManagementSystemContext>()
-                .Options;
-
-            // Створення двійників для DbContext та DbSet
-            var mockContext = new Mock<HRManagementSystemContext>(opt);
-            var mockDbSet = new Mock<DbSet<User>>();
-
-            mockContext
-                .Setup(context => context.Set<User>())
-                .Returns(mockDbSet.Object);
-
-            var repository = new BaseRepository<User>(mockContext.Object);
-            User expectedUser = new User { IdUser = 1, UserName = "Test User" };
-
-            // Act: виклик методу Create
-            repository.Create(expectedUser);
-
-            // Assert: перевірка очікуваної поведінки
-            mockDbSet.Verify(
-                dbSet => dbSet.Add(expectedUser),
-                Times.Once());
-        }
-
-
-        [Fact]
-        public void Get_InputId_CalledFindMethodOfDBSetWithCorrectId()
-        {
-            // Arrange: організація тестових даних
-            DbContextOptions opt = new DbContextOptionsBuilder<HRManagementSystemContext>()
-                .Options;
-
-            // Створення двійників для DbContext та DbSet
-            var mockContext = new Mock<HRManagementSystemContext>(opt);
-            var mockDbSet = new Mock<DbSet<UserRequest>>();
-
-            mockContext
-                .Setup(context => context.Set<UserRequest>())
-                .Returns(mockDbSet.Object);
-
-            var repository = new BaseRepository<UserRequest>(mockContext.Object);
-
-            UserRequest expectedRequest = new UserRequest { IdUserRequests = 1, IdUser = 42, CreatedAt = DateTime.Now };
-
-            // Налаштування двійника DbSet для методу Find
-            mockDbSet.Setup(dbSet => dbSet.Find(expectedRequest.IdUserRequests))
-                     .Returns(expectedRequest);
-
-            // Act: виклик методу Get
-            var actualRequest = repository.Get(expectedRequest.IdUserRequests);
-
-            // Assert: перевірка очікуваної поведінки
-            mockDbSet.Verify(
-                dbSet => dbSet.Find(expectedRequest.IdUserRequests),
-                Times.Once());
-
-            Assert.Equal(expectedRequest, actualRequest);
-        }
-
-        [Fact]
-        public void Delete_InputId_CalledFindAndRemoveMethodsOfDBSetWithCorrectArg()
-        {
-            // Arrange
-            DbContextOptions opt = new DbContextOptionsBuilder<HRManagementSystemContext>()
-                .Options;
-
-            // Створення двійників для DbContext та DbSet
-            var mockContext = new Mock<HRManagementSystemContext>(opt);
-            var mockDbSet = new Mock<DbSet<UserRequest>>();
-
-            mockContext
-                .Setup(context => context.Set<UserRequest>())
-                .Returns(mockDbSet.Object);
-
-            var repository = new BaseRepository<UserRequest>(mockContext.Object);
-
-            // Тестовий об'єкт UserRequest
-            UserRequest expectedUserRequest = new UserRequest
+            [Fact]
+            public void Create_InputUserInstance_CalledAddMethodOfDBSetWithUserInstance()
             {
-                IdUserRequests = 1,
-                IdUser = 42,
-                CreatedAt = DateTime.Now
-            };
+                // Arrange: організація тестових даних
+                DbContextOptions opt = new DbContextOptionsBuilder<HRManagementSystemContext>()
+                    .Options;
 
-            // Налаштування двійника DbSet для методу Find
-            mockDbSet.Setup(dbSet => dbSet.Find(expectedUserRequest.IdUserRequests))
-                     .Returns(expectedUserRequest);
+                // Створення двійників для DbContext та DbSet
+                var mockContext = new Mock<HRManagementSystemContext>(opt);
+                var mockDbSet = new Mock<DbSet<User>>();
 
-            // Act: виклик методу Delete
-            repository.Delete(expectedUserRequest.IdUserRequests);
+                mockContext
+                    .Setup(context => context.Set<User>())
+                    .Returns(mockDbSet.Object);
 
-            // Assert: перевірка очікуваної поведінки
-            mockDbSet.Verify(
-                dbSet => dbSet.Find(expectedUserRequest.IdUserRequests),
-                Times.Once());
+                var repository = new BaseRepository<User>(mockContext.Object);
+                User expectedUser = new User { IdUser = 1, UserName = "Test User" };
 
-            mockDbSet.Verify(
-                dbSet => dbSet.Remove(expectedUserRequest),
-                Times.Once());
+                // Act: виклик методу Create
+                repository.Create(expectedUser);
+
+                // Assert: перевірка очікуваної поведінки
+                mockDbSet.Verify(
+                    dbSet => dbSet.Add(expectedUser),
+                    Times.Once());
+            }
+
+
+            [Fact]
+            public void Get_InputId_CalledFindMethodOfDBSetWithCorrectId()
+            {
+                // Arrange: організація тестових даних
+                DbContextOptions opt = new DbContextOptionsBuilder<HRManagementSystemContext>()
+                    .Options;
+
+                // Створення двійників для DbContext та DbSet
+                var mockContext = new Mock<HRManagementSystemContext>(opt);
+                var mockDbSet = new Mock<DbSet<UserRequest>>();
+
+                mockContext
+                    .Setup(context => context.Set<UserRequest>())
+                    .Returns(mockDbSet.Object);
+
+                var repository = new BaseRepository<UserRequest>(mockContext.Object);
+
+                UserRequest expectedRequest = new UserRequest { IdUserRequests = 1, IdUser = 42, CreatedAt = DateTime.Now };
+
+                // Налаштування двійника DbSet для методу Find
+                mockDbSet.Setup(dbSet => dbSet.Find(expectedRequest.IdUserRequests))
+                         .Returns(expectedRequest);
+
+                // Act: виклик методу Get
+                var actualRequest = repository.Get(expectedRequest.IdUserRequests);
+
+                // Assert: перевірка очікуваної поведінки
+                mockDbSet.Verify(
+                    dbSet => dbSet.Find(expectedRequest.IdUserRequests),
+                    Times.Once());
+
+                Assert.Equal(expectedRequest, actualRequest);
+            }
+
+            [Fact]
+            public void Delete_InputId_CalledFindAndRemoveMethodsOfDBSetWithCorrectArg()
+            {
+                // Arrange
+                DbContextOptions opt = new DbContextOptionsBuilder<HRManagementSystemContext>()
+                    .Options;
+
+                // Створення двійників для DbContext та DbSet
+                var mockContext = new Mock<HRManagementSystemContext>(opt);
+                var mockDbSet = new Mock<DbSet<UserRequest>>();
+
+                mockContext
+                    .Setup(context => context.Set<UserRequest>())
+                    .Returns(mockDbSet.Object);
+
+                var repository = new BaseRepository<UserRequest>(mockContext.Object);
+
+                // Тестовий об'єкт UserRequest
+                UserRequest expectedUserRequest = new UserRequest
+                {
+                    IdUserRequests = 1,
+                    IdUser = 42,
+                    CreatedAt = DateTime.Now
+                };
+
+                // Налаштування двійника DbSet для методу Find
+                mockDbSet.Setup(dbSet => dbSet.Find(expectedUserRequest.IdUserRequests))
+                         .Returns(expectedUserRequest);
+
+                // Act: виклик методу Delete
+                repository.Delete(expectedUserRequest.IdUserRequests);
+
+                // Assert: перевірка очікуваної поведінки
+                mockDbSet.Verify(
+                    dbSet => dbSet.Find(expectedUserRequest.IdUserRequests),
+                    Times.Once());
+
+                mockDbSet.Verify(
+                    dbSet => dbSet.Remove(expectedUserRequest),
+                    Times.Once());
+            }
         }
     }
 }
